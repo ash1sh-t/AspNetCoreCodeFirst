3c19da4cd0c6ec616371449da4e70849721f5455
Ashish Oraon <1975463@SITEC_1433-03>
2021-08-24 20:23:58 +0530

    Key Annotations

--- AspNetCoreCodeFirst/Migrations/ContextModelSnapshot.cs
+++ AspNetCoreCodeFirst/Migrations/ContextModelSnapshot.cs
@@ -26,10 +26,14 @@ namespace AspNetCoreCodeFirst.Migrations
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
                     b.Property<string>("AuthorName")
+                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("BookName")
                         .HasColumnType("nvarchar(max)");
+
+                    b.Property<int>("DepartmentId")
+                        .HasColumnType("int");
 
                     b.Property<string>("language")
                         .HasColumnType("nvarchar(max)");
@@ -39,7 +43,35 @@ namespace AspNetCoreCodeFirst.Migrations
 
                     b.HasKey("Id");
 
+                    b.HasIndex("DepartmentId");
+
                     b.ToTable("Books");
+                });
+
+            modelBuilder.Entity("AspNetCoreCodeFirst.Models.Department", b =>
+                {
+                    b.Property<int>("DepartmentId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int")
+                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<string>("Name")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("DepartmentId");
+
+                    b.ToTable("Department");
+                });
+
+            modelBuilder.Entity("AspNetCoreCodeFirst.Models.Book", b =>
+                {
+                    b.HasOne("AspNetCoreCodeFirst.Models.Department", "Department")
+                        .WithMany()
+                        .HasForeignKey("DepartmentId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Department");
                 });
 #pragma warning restore 612, 618
         }
--- AspNetCoreCodeFirst/Models/Book.cs
+++ AspNetCoreCodeFirst/Models/Book.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AspNetCoreCodeFirst.Models
 {
     public class Book
     {
+        [Key]
         public int Id { get; set; }
         public string BookName { get; set; }
         [Required]
         public string AuthorName { get; set; }
         public double price { get; set; }
         public string language { get; set; }
+
         //public int Rating { get; set; }
     }
 }
