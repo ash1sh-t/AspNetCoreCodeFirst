bab2a1ff2306a5d90a198c69f4471c999ca29551
Ashish Oraon <1975463@SITEC_1433-03>
2021-08-24 20:06:37 +0530

    Data Annotation Added

--- AspNetCoreCodeFirst/Models/Book.cs
+++ AspNetCoreCodeFirst/Models/Book.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace AspNetCoreCodeFirst.Models
 {
@@ -9,6 +10,7 @@ namespace AspNetCoreCodeFirst.Models
     {
         public int Id { get; set; }
         public string BookName { get; set; }
+        [Required]
         public string AuthorName { get; set; }
         public double price { get; set; }
         public string language { get; set; }
